f126f98a1bc30794445cdb4f779104151881001f
Коляска _ <nikolaj.karpov.2002@mail.ru>
2022-03-21 23:21:50 +0500

    Path finding algorithm changed

--- DirectedGraphRunner/src/DirectedGraph.java
+++ DirectedGraphRunner/src/DirectedGraph.java
@@ -12,9 +12,7 @@ public class DirectedGraph {
     }
 
     public String[] getMaxPath(int start, int end) {
-        var path = new int[nodeCount];
-        Arrays.fill(path, -1);
-        path[0] = start - 1;
+        var path = new HashMap<Integer, Integer>();
 
         var optimal = new int[nodeCount];
         Arrays.fill(optimal, Integer.MIN_VALUE);
@@ -23,26 +21,32 @@ public class DirectedGraph {
         fillOptimalAndPath(optimal, path);
 
         var correctResult = new ArrayList<String>();
+
         if (optimal[end - 1] != Integer.MIN_VALUE) {
             var pathBuilder = new StringBuilder();
-            for (var node : Arrays.stream(path).distinct().toArray())
-                if (node != -1)
-                    pathBuilder.append(node + 1).append(" ");
-            correctResult.add(pathBuilder.toString());
+            pathBuilder.append(" ").append(end);
+
+            var currentLevel = path.get(end - 1);
+            for (var i = 0; i < path.size(); i++) {
+                pathBuilder.append(" ").append(currentLevel + 1);
+                currentLevel = path.get(currentLevel);
+                if (currentLevel == null)
+                    break;
+            }
+            correctResult.add(pathBuilder.reverse().toString());
             correctResult.add(Integer.toString(optimal[end - 1]));
         }
 
         return correctResult.toArray(new String[0]);
     }
 
-    private void fillOptimalAndPath(int[] optimal, int[] path) {
+    private void fillOptimalAndPath(int[] optimal, HashMap<Integer, Integer> path) {
         for (var pathSize = 1; pathSize <= nodeCount - 1; pathSize++)
             for (var edge : graph)
                 if (optimal[edge.from] != Integer.MIN_VALUE) {
                     if (optimal[edge.from] * edge.cost >= optimal[edge.to]) {
                         optimal[edge.to] = optimal[edge.from] * edge.cost;
-                        if (edge.from == pathSize - 1)
-                            path[pathSize] = edge.to;
+                        path.put(edge.to, edge.from);
                     }
                 }
     }
